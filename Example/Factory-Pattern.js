f3fb3ac33b42d5235fcdeb1de2b7d7a7e5075271
skillnull <skillisnull@gmail.com>
2019-09-02 10:46:17 +0800

    factory pattern examnple

--- Example/Factory-Pattern.js
+++ Example/Factory-Pattern.js
@@ -1,1 +1,33 @@
 // 工厂模式(Factory Pattern)
+
+let operationFactory = {
+    createOperate (operate) {
+        let oper = null
+        switch (operate) {
+            case '+':
+                oper = new operationFactory.OperationAdd()
+                break
+            case '-':
+                oper = new operationFactory.OperationSub()
+                break
+            case '*':
+                oper = new operationFactory.OperationMul()
+                break
+            case '/':
+                oper = new operationFactory.OperationDiv()
+                break
+        }
+    },
+    OperationAdd () {
+        // TODO run the addition operation
+    },
+    OperationSub () {
+        // TODO run the subtraction operation
+    },
+    OperationMul () {
+        // TODO run the multiplication operation
+    },
+    OperationDiv () {
+        // TODO run the division operation
+    }
+}
