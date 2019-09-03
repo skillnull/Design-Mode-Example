8debec87efe1d042ae5dc4ed888c9ffee080433e
skillnull <skillisnull@gmail.com>
2019-09-03 14:18:04 +0800

    stratery pattern 策略模式

--- Example/Strategy-Pattern.js
+++ Example/Strategy-Pattern.js
@@ -1,2 +1,25 @@
 // 策略模式(Strategy Pattern)
 
+// 策略算法封装
+let strategy = {
+    add (number1, number2) {
+        return number1 + number2
+    },
+    sub (number1, number2) {
+        return number1 - number2
+    },
+    mul (number1, number2) {
+        return number1 * number2
+    },
+    div (number1, number2) {
+        return number1 * number2
+    }
+}
+
+// 策略算法调用封装
+function callStratery (strategyType, number1, number2) {
+    return strategy[strategyType](number1, number2)
+}
+
+// 策略算法调用
+callStratery(add, 1, 2) // return 3
