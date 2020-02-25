a95a1792229952047b9cbb0f77a2e26cfdc10fa9
skillnull <skillisnull@gmail.com>
2020-02-25 17:39:18 +0800

    update

--- Example/Builder-Pattern.js
+++ Example/Builder-Pattern.js
@@ -1,1 +1,59 @@
 // 建造者模式(Builder Pattern)
+
+// 声明一个产品类
+class Product {
+    constructor () {
+
+    }
+}
+
+// 声明一个建造者类
+class BuilderProduct {
+    constructor () {
+        // 建造产品名称
+        this.nameBuilder = (name) => {
+            this.name = name || null
+        }
+        // 建造产品版本
+        this.versionBuilder = (version) => {
+            this.version = version || null
+        }
+        // 建造产品生产日期
+        this.createTimeBuilder = (createTime) => {
+            this.createTime = createTime || null
+        }
+        // 组合建造产品
+        this.getProduct = () => {
+            let product = new Product()
+            if (this.name) {
+                product.name = this.name
+            }
+
+            if (this.version) {
+                product.version = this.version
+            }
+
+            if (this.createTime) {
+                product.createTime = this.createTime
+            }
+
+            return product
+        }
+    }
+
+}
+
+// 声明一个需求类
+class Demand {
+    constructor (demands) {
+        let builderProduct = new BuilderProduct()
+        builderProduct.nameBuilder(demands.name)
+        builderProduct.versionBuilder(demands.version)
+        builderProduct.createTimeBuilder(demands.createTime)
+        return builderProduct.getProduct()
+    }
+}
+
+let demand = new Demand({
+    name: 'SKILL.NULL'
+})
