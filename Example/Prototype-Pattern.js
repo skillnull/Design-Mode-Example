1a0c399c690ee171eb3cdfd68c272a4d9014e11d
skillnull <skillisnull@gmail.com>
2020-02-27 14:47:58 +0800

    update Prototype Pattern

--- Example/Prototype-Pattern.js
+++ Example/Prototype-Pattern.js
@@ -1,1 +1,23 @@
 // 原型模式(Prototype Pattern)
+
+let productPrototype = {
+    init: (type) => {
+        this.type = type
+    },
+    getType: () => {
+        return this.type
+    }
+}
+let prototype = (type) => {
+    function F () {
+    }
+
+    F.prototype = productPrototype
+    let f = new F()
+    f.init(type)
+    return f
+}
+
+
+let car = prototype('丰田CHR')
+console.log(car.getType())
