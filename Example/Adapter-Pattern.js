ed84c505a10c51cc2a57feea86d54be0fda07113
skillnull <skillisnull@gmail.com>
2020-03-03 16:33:40 +0800

    Apapter Pattern JS demo

--- Example/Adapter-Pattern.js
+++ Example/Adapter-Pattern.js
@@ -1,1 +1,27 @@
 // 适配器模式(Adapter Pattern)
+
+class Target {
+    small () {
+        throw new Error('Please overwritten function!')
+    }
+}
+
+class newTarget {
+    big () {
+        console.log('新的适配')
+    }
+}
+
+class Adapter extends Target {
+    constructor (newTarget) {
+        super()
+        this.newTarget = newTarget
+    }
+    small () {
+        this.newTarget.big()
+    }
+}
+
+let NT = new newTarget()
+let AD = new Adapter(NT)
+AD.small()
