8753c12041be3d6bfa07835cf06187252bc4e191
skillnull <skillisnull@gmail.com>
2020-03-04 10:56:29 +0800

    Bridge Pattern

--- Example/Bridge-Pattern.js
+++ Example/Bridge-Pattern.js
@@ -1,1 +1,15 @@
 // 桥接模式(Bridge Pattern)
+
+let each = (arr, fn) => {
+    for (let i = 0; i < arr.length; i++) {
+        let val = arr[i]
+        if (fn.call(val, i, val, arr)) {
+            return false
+        }
+    }
+}
+
+let arr = [1, 2, 3, 4]
+each(arr, (i, v) => {
+    arr[i] = v * 2
+})
