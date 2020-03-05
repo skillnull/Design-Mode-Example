68a4523e41517980ff6e58e4d81575fcd175259b
skillnull <skillisnull@gmail.com>
2020-03-05 11:53:53 +0800

    Composite Pattern

--- Example/Composite-Pattern.js
+++ Example/Composite-Pattern.js
@@ -1,1 +1,41 @@
 // 组合模式(Composite Pattern)
+
+// 指令
+let directive = {
+    eat: {
+        excute: () => {
+            console.log('eat')
+        }
+    },
+    sleep: {
+        excute: () => {
+            console.log('sleep')
+        }
+    },
+    code: {
+        excute: () => {
+            console.log('Get out there and write code!')
+        }
+    }
+}
+
+const callDirective = () => {
+    let result = {
+        directiveList: [],
+        add: (directive) => {
+            result.directiveList.push(directive)
+        },
+        excute: () => {
+            for (let ele of result.directiveList.values()) {
+                ele.excute()
+            }
+        }
+    }
+    return result
+}
+
+let command = callDirective()
+command.add(directive.eat)
+command.add(directive.sleep)
+command.add(directive.code)
+command.excute()
