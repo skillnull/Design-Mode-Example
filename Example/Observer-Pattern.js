6feb2fcba513732897419dc0e4f81c139e4ecbf5
skillnull <skillisnull@gmail.com>
2020-03-12 20:08:58 +0800

    Observer Pattern

--- Example/Observer-Pattern.js
+++ Example/Observer-Pattern.js
@@ -1,1 +1,42 @@
 // 观察者模式(Observer Pattern)
+
+class Subject {
+    constructor () {
+        this.state = 0
+        this.observers = []
+    }
+
+    getState () {
+        return this.state
+    }
+
+    setState (state) {
+        this.state = state
+        this.notifyAllObsevers()
+    }
+
+    attach (observer) {
+        this.observers.push(observer)
+    }
+
+    notifyAllObsevers () {
+        for (let ele of this.observers.values()) {
+            ele.update()
+        }
+    }
+}
+
+class Observer {
+    constructor (name, subject) {
+        this.name = name
+        this.subject = subject
+        this.subject.attach(this)
+    }
+    update () {
+        console.log(this.name,this.subject.getState())
+    }
+}
+
+let subject = new Subject()
+let observer = new Observer('observer 1', subject)
+subject.setState('hahaha')
