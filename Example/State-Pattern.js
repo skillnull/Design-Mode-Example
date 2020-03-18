8cfaa78be65e4078007d0076c0cf4bf57e60c83f
skillnull <skillisnull@gmail.com>
2020-03-18 11:43:07 +0800

    State Pattern

--- Example/State-Pattern.js
+++ Example/State-Pattern.js
@@ -1,1 +1,39 @@
 // 状态模式(State Pattern)
+
+class SuperHero {
+    constructor () {
+        this.currentState = []
+        this.states = {
+            jump () {
+                console.log('jump')
+            },
+            move () {
+                console.log('move')
+            },
+            shoot () {
+                console.log('shoot')
+            },
+            squat () {
+                console.log('squat')
+            }
+        }
+    }
+
+    // 更改当前状态
+    change (arr) {
+        this.currentState = arr
+        return this
+    }
+
+    run () {
+        console.log('..... begin start ......')
+        this.currentState.forEach(item => this.states[item] && this.states[item]())
+        return this
+    }
+}
+
+new SuperHero()
+    .change(['jump', 'squat'])
+    .run()
+    .change(['move', 'shoot'])
+    .run()
