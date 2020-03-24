c70278bc48b2ead96eba9da747999adda91bbbf5
skillnull <skillisnull@gmail.com>
2020-03-24 19:28:02 +0800

    Command Pattern

--- Example/Command-Pattern.js
+++ Example/Command-Pattern.js
@@ -1,1 +1,42 @@
 // 命令模式(Command Pattern)
+
+let dialog = {
+    show () {
+        console.log('show a dialog')
+    }
+}
+
+let animation = {
+    start () {
+        console.log('show animation')
+    }
+}
+let setCommand = (btn, cmd) => {
+    btn.onclick = () => {
+        cmd.run()
+    }
+}
+
+class ShowDialogCommand {
+    constructor (receiver) {
+        this.receiver = receiver
+    }
+
+    run () {
+        this.receiver.start()
+    }
+}
+
+class StartAnimationCommand {
+    constructor (receiver) {
+        this.receiver = receiver
+    }
+
+    run () {
+        this.receiver.start()
+    }
+}
+
+
+setCommand(btn1, new ShowDialogCommand(dialog))
+setCommand(btn2, new StartAnimationCommand(animation))
