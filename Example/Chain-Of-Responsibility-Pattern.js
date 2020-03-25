629d14a59f929a6ccc87f3de66e5fa9e89556fb5
skillnull <skillisnull@gmail.com>
2020-03-25 17:36:40 +0800

    Chain of Responsibility Pattern

--- Example/Chain-Of-Responsibility-Pattern.js
+++ Example/Chain-Of-Responsibility-Pattern.js
@@ -1,1 +1,76 @@
 // 职责链模式(Chain of Responsibility Pattern)
+
+// 采购请求
+let PurchaseRequest = (amount, productName) => {
+    this.amount = amount
+    this.productName = productName
+}
+
+// 处理方
+let Approver = (name, nextApprover) => {
+    this.name = name
+    this.nextApprover = nextApprover
+}
+
+Approver.prototype.processRequest = () => {
+    throw new Error()
+}
+
+// ConcreteHandler
+let Manager = (name, nextApprover) => {
+    Approver.call(this, name, nextApprover)
+}
+extend(Manager, Approver)
+Manager.prototype.processRequest = (request) => {
+    if (request.Amount < 10000.0) {
+        console.log('ok')
+    } else if (NextApprover != null) {
+        this.nextApprover.ProcessRequest(request)
+    }
+}
+
+
+// ConcreteHandler,副总
+let VicePresident = function (name, nextApprover) {
+    Approver.call(this, name, nextApprover)
+}
+extend(VicePresident, Approver);
+VicePresident.prototype.processRequest = function (request) {
+    if (request.Amount < 25000.0) {
+        console.log('ok');
+    } else if (NextApprover != null) {
+        this.nextApprover.ProcessRequest(request)
+    }
+}
+
+
+// ConcreteHandler，总经理
+let President = function (name, nextApprover) {
+    Approver.call(this, name, nextApprover)
+}
+extend(President, Approver)
+President.prototype.processRequest = function (request) {
+    if (request.Amount < 100000.0) {
+        console.log('ok')
+    } else if (NextApprover != null) {
+        this.nextApprover.ProcessRequest(request)
+    }
+}
+
+
+let requestTelphone = new PurchaseRequest(4000.0, "Telphone")
+let requestSoftware = new PurchaseRequest(10000.0, "Visual Studio")
+let requestComputers = new PurchaseRequest(40000.0, "Computers")
+
+let manager = new Manager("LearningHard")
+let Vp = new VicePresident("Tony")
+let Pre = new President("BossTom")
+
+// 设置责任链
+manager.NextApprover = Vp
+Vp.NextApprover = Pre
+
+// 处理请求
+manager.ProcessRequest(requestTelphone)
+manager.ProcessRequest(requestSoftware)
+manager.ProcessRequest(requestComputers)
--- README.md
+++ README.md
@@ -125,6 +125,10 @@
 
 > [职责链模式(Chain of Responsibility Pattern)](/Example/Chain-Of-Responsibility-Pattern.js)
 
+    定义：为请求创建了一个接收者对象的链。
+    目的：避免请求发送者与接收者耦合在一起，让多个对象都有可能接收请求，将这些对象连接成一条链，并且沿着这条链传递请求，直到有对象处理它为止。
+    场景：红楼梦中的"击鼓传花"。
+    
 > [中介者模式(Mediator Pattern)](/Example/Memento-Pattern.js)
 
 > [解释器模式(Interpreter Pattern)](/Example/Iterator-Pattern.js)
