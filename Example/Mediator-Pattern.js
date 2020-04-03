94521334edee4f633d92fcc631782d7499cf9d9e
skillnull <skillisnull@gmail.com>
2020-04-03 19:34:11 +0800

    Mediator Pattern

--- Example/Mediator-Pattern.js
+++ Example/Mediator-Pattern.js
@@ -1,2 +1,26 @@
 // 中介者模式(Mediator Pattern)
 
+let mediator = (() => {
+    let msg = {}
+    return {
+        register: (type, action) => {
+            if (!msg[type]) msg[type] = []
+            msg[type].push(action)
+        },
+        send: (type) => {
+            if (msg[type]) {
+                for (let i = 0; i < msg[type].length; i++) {
+                    msg[type][i] && msg[type][i]()
+                }
+            }
+        }
+    }
+})()
+
+mediator.register('demo', () => {
+    console.log('first')
+})
+mediator.register('demo', () => {
+    console.log('second')
+})
+mediator.send('demo')
