9e75397342ca7c7f1ba184aab243eadf8b589079
skillnull <skillisnull@gmail.com>
2020-04-29 16:51:29 +0800

    Visitor Pattern

--- Example/Visitor-Pattern.js
+++ Example/Visitor-Pattern.js
@@ -1,1 +1,27 @@
 // 访问者模式(Visitor Pattern)
+
+// 元素类
+class Elements {
+    constructor () {
+        this.action = () => {
+            console.log('hi～')
+        }
+        this.accept = (visitor) => {
+            visitor.visit(this)
+        }
+    }
+}
+
+// 访问者
+class Visitor {
+    constructor () {
+        this.visit = (elements) => {
+            elements.action()
+        }
+    }
+}
+
+
+const ele = new Elements()
+const visi = new Visitor()
+ele.accept(visi)
