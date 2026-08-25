c6855a147db670b6c433cc657a4b3edd2e741928
SKILL.NULL <skillisnull@gmail.com>
2026-08-25 13:44:57 +0800

    Update Bridge-Pattern.js

--- Example/Bridge-Pattern.js
+++ Example/Bridge-Pattern.js
@@ -1,15 +1,49 @@
 // 桥接模式(Bridge Pattern)
 
-let each = (arr, fn) => {
-    for (let i = 0; i < arr.length; i++) {
-        let val = arr[i]
-        if (fn.call(val, i, val, arr)) {
-            return false
-        }
+// 实现部分
+class EmailSender {
+    send(content) {
+        console.log(`邮件发送：${content}`)
     }
 }
 
-let arr = [1, 2, 3, 4]
-each(arr, (i, v) => {
-    arr[i] = v * 2
-})
+class SmsSender {
+    send(content) {
+        console.log(`短信发送：${content}`)
+    }
+}
+
+// 抽象部分
+class Message {
+    constructor(sender) {
+        this.sender = sender
+    }
+
+    send(content) {
+        return this.sender.send(content)
+    }
+}
+
+class NormalMessage extends Message {
+    send(content) {
+        return this.sender.send(content)
+    }
+}
+
+class UrgentMessage extends Message {
+    send(content) {
+        return this.sender.send(`[紧急] ${content}`)
+    }
+}
+
+
+// 自由组合：
+
+// 邮件发送：会议通知
+const normalEmail = new NormalMessage(new EmailSender())
+normalEmail.send('会议通知')
+
+// 短信发送：[紧急] 服务器异常
+const urgentSms = new UrgentMessage(new SmsSender())
+urgentSms.send('服务器异常')
+
