68b5eb5c3ecb0e3f75f7a3c54ae8b353cf645911
SKILL.NULL <skillisnull@gmail.com>
2026-08-25 10:35:20 +0800

    Update Builder-Pattern.js

--- Example/Builder-Pattern.js
+++ Example/Builder-Pattern.js
@@ -1,59 +1,43 @@
 // 建造者模式(Builder Pattern)
 
-// 声明一个产品类
+ 定义：使用多个简单的对象一步一步构建成一个复杂的对象。
+ 目的：将一个复杂的构建与其表示相分离，使得同样的构建过程可以创建不同的表示。
+ 场景：一些基本部件不会变，而其组合经常变化的时候。
+
 class Product {
-    constructor () {
-
+    constructor() {
+        this.name = null
+        this.version = null
+        this.createTime = null
     }
 }
 
-// 声明一个建造者类
-class BuilderProduct {
-    constructor () {
-        // 建造产品名称
-        this.nameBuilder = (name) => {
-            this.name = name || null
-        }
-        // 建造产品版本
-        this.versionBuilder = (version) => {
-            this.version = version || null
-        }
-        // 建造产品生产日期
-        this.createTimeBuilder = (createTime) => {
-            this.createTime = createTime || null
-        }
-        // 组合建造产品
-        this.getProduct = () => {
-            let product = new Product()
-            if (this.name) {
-                product.name = this.name
-            }
-
-            if (this.version) {
-                product.version = this.version
-            }
-
-            if (this.createTime) {
-                product.createTime = this.createTime
-            }
-
-            return product
-        }
+class ProductBuilder {
+    constructor() {
+        this.product = new Product()
     }
 
-}
+    setName(name) {
+        this.product.name = name ?? null
+        return this
+    }
 
-// 声明一个需求类
-class Demand {
-    constructor (demands) {
-        let builderProduct = new BuilderProduct()
-        builderProduct.nameBuilder(demands.name)
-        builderProduct.versionBuilder(demands.version)
-        builderProduct.createTimeBuilder(demands.createTime)
-        return builderProduct.getProduct()
+    setVersion(version) {
+        this.product.version = version ?? null
+        return this
+    }
+
+    setCreateTime(createTime) {
+        this.product.createTime = createTime ?? null
+        return this
+    }
+
+    build() {
+        const result = this.product
+
+        // 让当前建造者可以继续构建下一个独立产品
+        this.product = new Product()
+
+        return result
     }
 }
-
-let demand = new Demand({
-    name: 'SKILL.NULL'
-})
