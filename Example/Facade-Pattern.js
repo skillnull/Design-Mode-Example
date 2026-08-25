8fad857320bbfdc33c7a37701fdb78465df403cb
SKILL.NULL <skillisnull@gmail.com>
2026-08-25 14:18:21 +0800

    Update Facade-Pattern.js

--- Example/Facade-Pattern.js
+++ Example/Facade-Pattern.js
@@ -1,9 +1,24 @@
 // 外观模式(Facade Pattern)
 
 // 通过一个接口封装其他接口
-let mobileEvent = {
-    stop: (e) => {
-        e.preventDefault()
-        e.stopPropagation()
+const mobileEvent = {
+    stop(event) {
+        if (
+            typeof event?.preventDefault !== 'function' ||
+            typeof event?.stopPropagation !== 'function'
+        ) {
+            throw new TypeError('必须传入有效的事件对象')
+        }
+
+        event.preventDefault()
+        event.stopPropagation()
     }
 }
+
+element.addEventListener(
+    'touchmove',
+    event => {
+        mobileEvent.stop(event)
+    },
+    { passive: false }
+)
