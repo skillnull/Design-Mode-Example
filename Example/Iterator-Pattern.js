afcff5173848aab814b2f17639d77eeed0746791
SKILL.NULL <skillisnull@gmail.com>
2026-08-25 19:54:39 +0800

    Enhance 'each' function for iterables with error handling
    Refactor 'each' function to support iterable objects and add error handling for invalid inputs.

--- Example/Iterator-Pattern.js
+++ Example/Iterator-Pattern.js
@@ -1,15 +1,28 @@
 // 迭代器模式(Iterator Pattern)
 
-let each = (arr, callBack) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (callBack && callBack(i, arr[i]) === false) {
-            break;
-        }
+function each(iterable, callback) {
+    if (iterable == null || typeof iterable[Symbol.iterator] !== 'function') {
+        throw new TypeError('第一个参数必须是可迭代对象')
     }
+
+    if (typeof callback !== 'function') {
+        throw new TypeError('callback 必须是函数')
+    }
+
+    let index = 0
+
+    for (const value of iterable) {
+        if (callback(index, value, iterable) === false) {
+            break
+        }
+
+        index++
+    }
+
+    return iterable
 }
 
-let arr = [1, 2, 3, 4]
-each(arr, (index, value) => {
-    if (value > 2) return false
+// 使用：
+each(new Set(['a', 'b', 'c']), (index, value) => {
     console.log(index, value)
 })
