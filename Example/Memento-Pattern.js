804df26cc3dc5d036f0e0256222f5fc583998634
SKILL.NULL <skillisnull@gmail.com>
2026-08-25 19:41:48 +0800

    Refactor getPageData function with caching improvements
    Refactor getPageData to use a Map for caching and support async data loading.

--- Example/Memento-Pattern.js
+++ Example/Memento-Pattern.js
@@ -1,21 +1,34 @@
 // 备忘录模式(Memento Pattern)
 
-let getPageData = (() => {
-        let cache = {}
-        return (page, fn) => {
-            if (cache[page]) {
-                // 已有指定缓存数据
-                fun && fn()
-            } else {
-                // 当前没有指定缓存数据
-                // 获取指定数据data
-                cache[page] = data
-                fn && fn()
-            }
-        }
-    }
-)()
+function createPageCache() {
+    const cache = new Map()
 
-getPageData(1, () => {
-    // do somthing
-})
+    return async function getPageData(page, loader) {
+        if (cache.has(page)) {
+            return cache.get(page)
+        }
+
+        const data = await loader(page)
+        cache.set(page, data)
+
+        return data
+    }
+}
+
+const getPageData = createPageCache()
+
+// 使用：
+async function fetchPageData(page) {
+    console.log(`请求第 ${page} 页数据`)
+
+    return {
+        page,
+        list: ['数据1', '数据2']
+    }
+}
+
+const data1 = await getPageData(1, fetchPageData)
+const data2 = await getPageData(1, fetchPageData)
+
+console.log(data1)
+console.log(data2)
