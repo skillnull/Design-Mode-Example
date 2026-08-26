04e3093766b8d8c272649f7adae91cd0f0aec82b
SKILL.NULL <skillisnull@gmail.com>
2026-08-26 14:09:44 +0800

    Update Mediator-Pattern.js

--- Example/Mediator-Pattern.js
+++ Example/Mediator-Pattern.js
@@ -1,26 +1,52 @@
 // 中介者模式(Mediator Pattern)
 
-let mediator = (() => {
-    let msg = {}
-    return {
-        register: (type, action) => {
-            if (!msg[type]) msg[type] = []
-            msg[type].push(action)
-        },
-        send: (type) => {
-            if (msg[type]) {
-                for (let i = 0; i < msg[type].length; i++) {
-                    msg[type][i] && msg[type][i]()
-                }
-            }
+// 中介者：统一协调跑道
+const tower = {
+    currentPlane: null,
+
+    requestLanding(plane) {
+        if (this.currentPlane) {
+            console.log(`${plane.name}：跑道被占用，请等待`)
+            return
+        }
+
+        this.currentPlane = plane
+        plane.land()
+    },
+
+    releaseRunway(plane) {
+        if (this.currentPlane === plane) {
+            this.currentPlane = null
         }
     }
-})()
+}
 
-mediator.register('demo', () => {
-    console.log('first')
-})
-mediator.register('demo', () => {
-    console.log('second')
-})
-mediator.send('demo')
+// 参与者：只与塔台通信，不直接联系其他飞机
+class Plane {
+    constructor(name, tower) {
+        this.name = name
+        this.tower = tower
+    }
+
+    requestLanding() {
+        this.tower.requestLanding(this)
+    }
+
+    land() {
+        console.log(`${this.name}：开始降落`)
+    }
+
+    leaveRunway() {
+        this.tower.releaseRunway(this)
+    }
+}
+
+const planeA = new Plane('飞机A', tower)
+const planeB = new Plane('飞机B', tower)
+
+planeA.requestLanding() // 飞机A：开始降落
+planeB.requestLanding() // 飞机B：跑道被占用，请等待
+
+planeA.leaveRunway()
+
+planeB.requestLanding() // 飞机B：开始降落
